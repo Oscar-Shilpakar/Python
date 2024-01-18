8cdd967e09639222340dd0260a508d53a1d4943c
Oscar-Shilpakar <ospakar0001@gmail.com>
2024-01-18 13:25:52 +0545

    Day 2 updated

--- Day2.py
+++ Day2.py
@@ -15,12 +15,33 @@
 -use replace() to replace string
 -use name[::-1] to reverse a string
 """
-name=input("Enter your name\n")
-print(name.split(" "))
-print(len(name)) 
-splitted_text=name.split(" ")
-print(f"First name is {splitted_text[0]}")
-print(f"Middle name is {splitted_text[1]}")
-print(f"Last name is {splitted_text[2]}")
-print(name.replace("oscar","pratistha"))
-print(name[::-1])
+# name=input("Enter your name\n")
+# print(name.split(" "))
+# print(len(name)) 
+# splitted_text=name.split(" ")
+# print(f"First name is {splitted_text[0]}")
+# print(f"Middle name is {splitted_text[1]}")
+# print(f"Last name is {splitted_text[2]}")
+# print(name.replace("oscar","pratistha"))
+# print(name[::-1])
+
+# girlfriends=["one","and","only","the","great","pratistha"]
+# print(f"my girlfriend {girlfriends}")
+# girlfriend_count=len(girlfriends)
+# print(girlfriend_count)
+
+# girlfriends=["one","and","only","the","great","pratistha"]
+# for i in girlfriends:
+#     print(i)
+# i=0
+# while(i<=5):
+#     print(i)
+#     i+=1
+
+"""WAP in such a way that when user enters input 'ram' as a name break loop and print the list otherwise keep on appending items to list amd ask for next input from terminal"""
+
+name_list=[""]
+while(name_list[-1]!="ram"):
+    name=input("Enter name: ")
+    name_list.append(name)
+print(name_list[1:])
