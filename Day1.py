4e86c14a759f856ce812f364489c0786cd76eb58
Oscar-Shilpakar <ospakar0001@gmail.com>
2024-01-18 11:56:51 +0545

    day1 commit

---
+++ Day1.py
@@ -0,0 +1,29 @@
+# Python basics
+
+# a = int(input("Please enter value for a: \n"))
+# b = int(input("Please enter value for b: \n"))
+# name = input("What is your name\n")
+# print(name)
+# breakpoint()
+# name = input("What is your name")
+# breakpoint()
+
+# name=input("Enter your name:\n")
+# address=input("Enter your address:\n")
+# phone_no=input("Enter you phone number:\n")
+# print(name)
+# print(address)
+# print(phone_no)
+
+num1=int(input("Enter number 1:\n"))
+num2=int(input("Enter number 2:\n"))
+print(f"sum of {num1} and {num2} is {num1+num2}")
+print(f"subtraction of {num1} and {num2} is {num1-num2}")
+print(f"division of {num1} and {num2} is {num1/num2}")
+print(f"multiplication of {num1} and {num2} is {num1*num2}")
+print(f"power of {num1} and {num2} is {pow(num1,num2)}")
+print(f"floor division of {num1} and {num2} is {num1//num2}")
+if(num1>num2):
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
