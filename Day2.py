88d77700e54ed6f1d757efcd8975e7f05cbd1b4a
Oscar-Shilpakar <ospakar0001@gmail.com>
2024-01-18 12:46:22 +0545

    Day 2 updated

--- Day2.py
+++ Day2.py
@@ -8,13 +8,19 @@
 
 # name="Oscar Shilpakar"
 
-# use print(varaible_name[a:b]) to print required string where a and b are starting point and ending point respectively
-# use print(variable_name.split(" ")) to split given string
-# use variable_name[a] to check what value is in the a position 
-
+"""-use print(varaible_name[a:b]) to print required string where a and b are starting point and ending point respectively
+-use print(variable_name.split(" ")) to split given string
+-use variable_name[a] to check what value is in the a position 
+-use len(variable name) is used to find the length of the string
+-use replace() to replace string
+-use name[::-1] to reverse a string
+"""
 name=input("Enter your name\n")
 print(name.split(" "))
+print(len(name)) 
 splitted_text=name.split(" ")
 print(f"First name is {splitted_text[0]}")
 print(f"Middle name is {splitted_text[1]}")
 print(f"Last name is {splitted_text[2]}")
+print(name.replace("oscar","pratistha"))
+print(name[::-1])
